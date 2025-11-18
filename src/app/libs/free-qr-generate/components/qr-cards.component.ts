7e534be97235b709d6bc2436f0cc7c874a51711e
MadAngular <roro555000ro@gmail.com>
2025-11-18 15:37:58 +0300

    Update qr-cards.component.ts

--- src/app/libs/free-qr-generate/components/qr-cards.component.ts
+++ src/app/libs/free-qr-generate/components/qr-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
